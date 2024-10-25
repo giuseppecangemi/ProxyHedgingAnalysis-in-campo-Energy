3605ec3faaca5bb9a5e079114676e72247a99af5
Cangemi <giucan03@protiviti.com>
2024-10-25 17:54:51 +0200

    Aggiornamento codice

--- basis_risk.py
+++ basis_risk.py
@@ -8,8 +8,8 @@ def rischio_base(data, spot, future):
     data.columns = ['Gas_Spot', 'Future_TTF', 'UK_Future', 'NG_Future', 'Brent_Future', 'WTI_Future', 'Heating_Oil', 'Gasoline']
 
     #calcolo la volatilità annualizzata
-    volatilità_spot = data[spot].pct_change().std() * np.sqrt(252) 
-    volatilità_future = data[future].pct_change().std() * np.sqrt(252) 
+    volatilità_spot = data[spot].pct_change().std() #* np.sqrt(252) 
+    volatilità_future = data[future].pct_change().std() #* np.sqrt(252) 
 
     print("ANALISI BASIS RISK | " f"Volatilità Spot: {volatilità_spot:.4f}, Volatilità Future: {volatilità_future:.4f}")
 
@@ -19,6 +19,19 @@ def rischio_base(data, spot, future):
     #stimo la volatilità della base
     basis_volatility = data['Basis'].std()
     print(f"Volatilità del Basis: {basis_volatility:.2f}")
+    #questa formula calcola solo la deviazione standard del differenziale
+    #per calcolare la varianza del basis considerando anche la correlazione e le deviazioni standard individuali dei prezzi spot e future devo modificarla come segue:
+    sigma_S = data[spot].std()
+    sigma_F = data[future].std()
+
+    #calcolo la correlazione tra spot e future
+    rho = data[spot].corr(data[future])
+
+    # Calcolo della varianza del basis
+    basis_variance = sigma_S**2 + sigma_F**2 - 2 * rho * sigma_S * sigma_F
+
+    print(f"++++++++++++Varianza del Basis: {basis_variance:.4f}")
+
 
     #plotto la base nel tempo -> più bassa è meglio è
     plt.figure(figsize=(12, 6))
@@ -31,7 +44,7 @@ def rischio_base(data, spot, future):
     plt.grid()
     plt.show()
 
-    #plotto la distribuzione della base. valori vicino lo zero sono preferiti
+    #plotto la distribuzione della base. valori vicino lo zero sono
     plt.figure(figsize=(12, 6))
     plt.hist(data['Basis'], bins=30, alpha=0.7, color='blue', edgecolor='black')
     plt.title('Distribuzione del Basis')
--- main.py
+++ main.py
@@ -2,27 +2,30 @@ from caricamento_dati import variabili
 from correlazione import correlation  
 from basis_risk import rischio_base
 from condizioni_mercato import condizioni_mercato
-from hedge_ratio import calcola_hedge_ratio    
+from OLS_hedge_ratio import OLS_hedge_ratio    
 from stazionarieta import stazionarieta
 from CCGARCH_hedge_ratio import calcola_rendimenti, ccgarch_hedge_ratio
 from DCC_GARCH_hedge_ratio import dcc_garch_hedge_ratio
+from Conditional_OLS_hedge_ratio import conditional_OLS_hedge_ratio
 
 spot = 'Gas_Spot'
 future = 'Future_TTF'
-log = "si"
+log = "no"
 def main():
     dati = variabili()
     print(dati)
     #calcolo e view matrice correlazione
-    correlation_matrix = correlation(dati)
+    #correlation_matrix = correlation(dati)
     #sstampo la matrice
-    print(correlation_matrix)
+    #print(correlation_matrix)
     #calcolo volatilita basis Risk:
-    rischio_base(dati, spot, future)
+    #rischio_base(dati, spot, future)
     # calcolo condizioni di mercato
-    condizioni_mercato(dati, spot, future) 
-    #hedge ratio e test eteroschedasticità
-    #calcola_hedge_ratio(spot, future, log)
+    #condizioni_mercato(dati, spot, future) 
+    #OLS hedge ratio e test eteroschedasticità
+    OLS_hedge_ratio(spot, future, log)
+    #CONDITIONAL OLS HEDGE RATIO
+    #conditional_OLS_hedge_ratio(spot, future, log)
     #stzionarietà
     #stazionarieta(dati, spot, future) 
     #Constant Correlation GARCH
